5b59e63f1eb4e0e470186fd90b8f2b0ca41a93fc
Karam Kabbara <contact@karam.io>
2020-10-22 19:07:57 +0100

    Initial repo setup

---
+++ Builder Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Builder_Pattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
